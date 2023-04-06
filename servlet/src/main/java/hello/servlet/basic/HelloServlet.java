e56c6c470f02d07e2cf78a114cd799c951abbeeb
SangBeom-Hahn <hsb990917@gmail.com>
2023-04-07 07:44:59 +0900

    http request message intro

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String name = req.getParameter("name");
+        System.out.println("name = " + name);
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("hello  " + name);
+    }
+}

b8d93c6c3645797b7a142ce28ededc3820ef39be
SangBeom-Hahn <hsb990917@gmail.com>
2023-04-21 23:36:00 +0900

    bean valid finish

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,15 +10,15 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
-    @NotNull(groups = UpdateCheck.class) // 수정
+//    @NotNull(groups = UpdateCheck.class) // 수정
     private Long id;
-    @NotBlank(message = "공백 X", groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(message = "공백 X", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {

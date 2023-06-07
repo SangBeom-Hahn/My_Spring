e13301c9e05cd7dfea2d3f99748fffd59fb845b0
SangBeom-Hahn <hsb990917@gmail.com>
2023-06-07 12:01:19 +0900

    jpa start

---
+++ hellojpa/src/main/java/jpabasic/hellojpa/HellojpaApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.hellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellojpaApplication.class, args);
+	}
+
+}
---
+++ hellojpa/src/test/java/jpabasic/hellojpa/HellojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

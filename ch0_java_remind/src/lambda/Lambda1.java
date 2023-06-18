fed88f298f2f6fc3714ea614812240caadca36d7
SangBeom-Hahn <hsb990917@gmail.com>
2023-06-18 09:36:28 +0900

    lamnda

--- ch0_java_remind/src/lambda/Lambda1.java
+++ ch0_java_remind/src/lambda/Lambda1.java
@@ -4,5 +4,13 @@ public class Lambda1 {
 
     public int max(int a, int b) {
         return a > b ? a : b;
-    } 
+    }
+
+    Object o = (int a, int b) -> a > b ? a : b
+
+    new Object() {
+        int max(int a, int b) {
+            return a > b ? a : b;
+        }
+    }
 }

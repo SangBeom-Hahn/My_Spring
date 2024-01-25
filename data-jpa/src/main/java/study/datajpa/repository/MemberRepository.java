07c96b20d255eb90de974cf2cb2ce91343c86daa
SangBeom-Hahn <hsb990917@gmail.com>
2024-01-25 13:05:39 +0900

    paging

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,10 +1,34 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     public List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    // 이름이 emt도 되면 opt
+    List<Member> findListByUsername(String name);
+    Member findOneByUsername(String name);
+    Optional<Member> findOptByUsername(String name);
+
+    @Query(value = "select m from Member m left join m.team t",
+    countQuery = "select count(m) from Member m")
+    List<Member> findByAge(int age, Pageable pageable);
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,9 +48,136 @@ class MemberJpaRepositoryTest {
         // when
         memberRepository.save(member);
         memberRepository.save(member1);
-        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+//        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+//
+//        // then
+//        Assertions.assertThat(members.get(0).getAge()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("")
+    void test() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+
+        // when
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+//        List<Member> result = memberRepository.findUser("AAA", 10);
+//
+//        // then
+//        assertThat(result.get(0)).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("")
+    void test1() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+
+        // when
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+//        List<String> usernameList = memberRepository.findUsernameList();
 
         // then
-        Assertions.assertThat(members.get(0).getAge()).isEqualTo(20);
+//        for (String s : usernameList) {
+//            System.out.println(s);
+//        }
+    }
+
+    @Test
+    @DisplayName("")
+    void test2() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("BBB", 20);
+
+        // when
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+//        List<Member> names = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+//
+//        for (Member name : names) {
+//            System.out.println(name);
+//        }
+    }
+
+    @Test
+    @DisplayName("")
+    void test3() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+
+        // when
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+//        Optional<Member> aaab = memberRepository.findOptByUsername("AAA");
+//        System.out.println("result =" + aaab);
+    }
+
+    @Test
+    @DisplayName("")
+    void test4() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 10);
+        Member member3 = new Member("AAA", 10);
+        Member member4 = new Member("AAA", 10);
+
+        memberRepository.save(member);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+//        List<Member> members = memberRepository.findByPage(age, offset, limit);
+//        long totalCount = memberRepository.totalCount(age);
+//
+//        // then
+//        assertThat(members.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("")
+    void test5() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 10);
+
+        memberRepository.save(member);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        int age = 10;
+        PageRequest pageRequest =
+                PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        // when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        page.map(member3 -> new MemberDto(member3.getTeam(), member3.getAge()))
+
+        // then
+        List<Member> content = page.getContent();
+//        long totalElements = page.getTotalElements();
+
+        for (Member member3 : content) {
+            System.out.println("member3 = " + member3);
+        }
+//        System.out.println("totalElements = " + totalElements);
     }
 }
